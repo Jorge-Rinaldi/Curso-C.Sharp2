fea4b6b421fcc64cc7d120cc4e652e5ef7658394
Jorge-Rinaldi <jorgelrinaldi.jr@gmail.com>
2023-08-26 22:59:16 -0300

    segunda vez

---
+++ Desafio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono T1 = new Telefono("GS200", "Samsung");
+            Console.WriteLine("El modelo del telefono es: " + T1.modelo + " y la marca es: " + T1.marca);
+            Console.WriteLine("Ingrese el telefono al que desea llamar:");
+            T1.NumTelefono = (Console.ReadLine());
+            Console.WriteLine("Ingrese el codigo de operador:");
+            T1.CodOper = int.Parse(Console.ReadLine());
+            Console.WriteLine(T1.Llamar());
+            Console.WriteLine(T1.Llamar("Negocio"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Desafio1/Telefono.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Telefono
+    {
+        public Telefono(string Modelo, string Marca)
+        {
+            this.Modelo = Modelo;
+            this.Marca = Marca;
+        }
+
+        private string Modelo;
+        private string Marca;
+        private string NumeroTelefonico;
+        private int CodigoOperador;
+
+        public string modelo { get; set; }
+        public string marca { get; set; }
+
+        public string NumTelefono { get; set; }
+
+        public int CodOper
+        {
+            get { return CodigoOperador; }
+            set
+            {
+                if (CodigoOperador == 1)
+                    CodigoOperador = value;
+                else
+                {
+                    value = 0;
+                }
+            }
+        }
+        public string Llamar()
+        {
+            return "Realizando llamada...";
+        }
+
+        public string Llamar(string Contacto)
+        {
+            return "Llamando a... " + Contacto;
+        }
+    }
+}
